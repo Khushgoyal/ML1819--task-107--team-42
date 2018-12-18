42bf72ee3319a1078d07ddb689ffbb7dc723acfd
mandashilpa26 <mandas@tcd.ie>
2018-12-18 22:36:57 +0000

    model building 2

--- ModelBuilding.py
+++ ModelBuilding.py
@@ -1,11 +1,11 @@
 """ * Gender prediction using Twitter data *
 
+This code builds the data model with the data present in twitter_data_model_duilding.csv
 
-
-
-
-
-
+Three algorithms are applied here:
+1.Logistic Regression( regularised)
+2.Support Vector Machine
+3.RandomForest
 
 """
 from sklearn.model_selection import train_test_split
@@ -100,3 +100,74 @@ def PerformanceEvaluationMetrics(y_test,y_pred):
     plt.title('Receiver operating characteristic example')
     plt.legend(loc="lower right")
     plt.show()
+
+	#Function for 90% winsorization 
+def winsorizarion_90(series):
+    low_limit=series.quantile(0.05)
+    high_limit=series.quantile(0.95)
+    series[series<low_limit]=low_limit
+    series[series>high_limit]=high_limit
+    return series  
+
+#90% winsorization 
+twitter_data.loc[:,twitter_data.apply(lambda x: x.nunique()) > 2] = twitter_data.loc[:,twitter_data.apply(lambda x: x.nunique()) > 2].apply(winsorizarion_90)
+
+    
+#Twitter Data Scaling and Diving data into Test and Train
+x = twitter_data.drop('gender',axis=1)
+y = twitter_data.gender
+colnames=x.columns 
+sc = StandardScaler()  
+X  = sc.fit_transform(x)  
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+
+
+
+###############################Logistic Regression###################################### 
+#Logistic regression
+# Make the instance of the model
+#log_coeff=pd.DataFrame(model_col,columns=['log_coeff'])
+
+logisticRegrModel = LogisticRegression(random_state = 2018)
+logisticRegrModel.fit(X,y)
+#logisticPredictions = logisticRegrModel.predict(X_test)
+#PerformanceEvaluationMetrics(y_test,logisticPredictions)
+
+
+params = {"C": [0.005,0.01,0.05,0.1,0.5,1,1.5,2.5,5,10]}
+Log_model_cv = GridSearchCV(estimator = logisticRegrModel, param_grid = params, 
+                        scoring= ['accuracy','roc_auc','f1'], 
+                        cv = 10, 
+                        verbose = 4,
+                       refit=False,return_train_score = True)
+Log_model_cv.fit(X,y)
+log_cv_results=pd.DataFrame(Log_model_cv.cv_results_)
+log_cv_results
+
+best_accuracy = log_cv_results.mean_test_accuracy.idxmax()
+print(round(pd.DataFrame(Log_model_cv.cv_results_).mean_test_accuracy[best_accuracy],3))
+print(round(pd.DataFrame(Log_model_cv.cv_results_).std_test_accuracy[best_accuracy],3))
+#0.639
+#0.013
+
+print(round(pd.DataFrame(Log_model_cv.cv_results_).mean_test_roc_auc[best_accuracy],3))
+print(round(pd.DataFrame(Log_model_cv.cv_results_).std_test_roc_auc[best_accuracy],3))
+#0.695
+#0.014
+
+#print(round(pd.DataFrame(Log_model_cv.cv_results_).mean_test_f1[best_accuracy],3))
+#print(round(pd.DataFrame(Log_model_cv.cv_results_).std_test_f1[best_accuracy],3))
+#0.636
+#0.032
+
+
+logisticRegrModel = LogisticRegression(random_state = 2018,C=0.01)
+logisticRegrModel.fit(X,y)
+print(logisticRegrModel.coef_)
+
+co=logisticRegrModel.coef_.reshape(63,)
+feat_importances_lg=pd.DataFrame({'features':x.columns ,'logistic_regression_coeff':co})
+feat_importances_lg=feat_importances_lg.sort_values('logistic_regression_coeff')
+feat_importances_lg.plot.barh(x='features',y='logistic_regression_coeff',figsize=(15,10))
+
+
